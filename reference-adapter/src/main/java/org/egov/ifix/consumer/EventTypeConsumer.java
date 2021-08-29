0e6c8ede51c3b7de3430ebce63992d4ba96ff08c
Manikanta <manikanta@egovernments.org>
2021-08-30 00:33:13 +0530

    [IFIX-148]  adding proper response

--- reference-adapter/src/main/java/org/egov/ifix/consumer/EventTypeConsumer.java
+++ reference-adapter/src/main/java/org/egov/ifix/consumer/EventTypeConsumer.java
@@ -1,5 +1,6 @@
 package org.egov.ifix.consumer;
 
+import java.time.Instant;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
@@ -21,6 +22,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
@@ -30,6 +32,16 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class EventTypeConsumer {
 
+	private static final String MSG_ID = "msgId";
+
+	private static final String VERSION = "version";
+
+	private static final String REQUEST_HEADER = "requestHeader";
+
+	private static final String EVENT_TYPE = "eventType";
+
+	private static final String EVENT = "event";
+
 	private List<EventMapper> eventMappers;
 
 	private Map<String, EventMapper> eventTypeMap = new HashMap<>();
@@ -38,78 +50,75 @@ public class EventTypeConsumer {
 
 	private EventPostingDetailRepository eventPostingDetailRepository;
 
-
 	@Autowired
-	public EventTypeConsumer(List<EventMapper> eventMappers,PostEvent postEvent,EventPostingDetailRepository eventPostingDetailRepository) {
+	public EventTypeConsumer(List<EventMapper> eventMappers, PostEvent postEvent,
+			EventPostingDetailRepository eventPostingDetailRepository) {
 		this.eventMappers = Collections.unmodifiableList(eventMappers);
-		this.postEvent=postEvent;
-		this.eventPostingDetailRepository=eventPostingDetailRepository;
+		this.postEvent = postEvent;
+		this.eventPostingDetailRepository = eventPostingDetailRepository;
 		initializeEventTypeMap();
 
 	}
 
 	private void initializeEventTypeMap() {
-		for (EventMapper eventMapper : eventMappers) 
+		for (EventMapper eventMapper : eventMappers)
 			eventTypeMap.put(eventMapper.getEventType(), eventMapper);
 	}
 
 	@KafkaListener(topics = "${kafka.topics.ifix.adaptor.mapper}")
-	public void listen(final String record)  {
+	public void listen(final String record) {
 		log.info("Received Message in group foo: " + record);
 		JsonObject jsonObject = JsonParser.parseString(record).getAsJsonObject();
-		EventMapper eventMapper = eventTypeMap.get(jsonObject.getAsJsonObject("event").get("eventType").getAsString());
+		EventMapper eventMapper = eventTypeMap.get(jsonObject.getAsJsonObject(EVENT).get(EVENT_TYPE).getAsString());
 		List<FiscalEvent> fiscalEvents = eventMapper.transformData(jsonObject);
-		FiscalEventRequest request=new FiscalEventRequest();
-		RequestHeader header=new RequestHeader();
-		header.setVersion("1.0");
-		header.setMsgId("1.0");
-		header.setTs(1111l);
+		FiscalEventRequest request = new FiscalEventRequest();
+		RequestHeader header = new RequestHeader();
+		if (jsonObject.get(REQUEST_HEADER) != null) {
+			JsonElement requestHeader = jsonObject.get(REQUEST_HEADER);
+			JsonObject requestJsonObject = requestHeader.getAsJsonObject();
+			if (requestJsonObject.get(VERSION) != null) {
+				header.setVersion(requestJsonObject.get(VERSION).toString());
+			}
+			if (requestJsonObject.get(MSG_ID) != null) {
+				header.setMsgId(requestJsonObject.get(MSG_ID).toString());
+			}
+			header.setTs(Instant.now().toEpochMilli());
+		}
+
 		request.setRequestHeader(header);
 
- loop:		for(FiscalEvent event:fiscalEvents)
-		{
-	      
-	      if(jsonObject.get("projectId")!=null)
-	      {
-	    	  event.setProjectId(jsonObject.get("projectId").toString());
-	      }
+		loop: for (FiscalEvent event : fiscalEvents) {
+
+			if (jsonObject.get("projectId") != null) {
+				event.setProjectId(jsonObject.get("projectId").toString());
+			}
 
 			request.setFiscalEvent(event);
 
-			/*Gson gson = new Gson();
-	    String json = gson.toJson(fiscalEvents);
-	    log.info(json);*/
-			try{
+			try {
 				ResponseEntity<FiscalEventResponse> response = postEvent.post(request);
-				if(response.getStatusCode().series().equals(HttpStatus.Series.SERVER_ERROR))
-				{
+				if (response.getStatusCode().series().equals(HttpStatus.Series.SERVER_ERROR)) {
 
-					EventPostingDetail detail=new EventPostingDetail
-							( null,event.getTenantId(),event.getReferenceId(),event.getEventType(),response.getStatusCode().toString(),null,record
-									,new Date(),new Date());
+					EventPostingDetail detail = new EventPostingDetail(null, event.getTenantId(),
+							event.getReferenceId(), event.getEventType(), response.getStatusCode().toString(), null,
+							record, new Date(), new Date());
 					eventPostingDetailRepository.save(detail);
-				}else
-				{
-					EventPostingDetail detail=new EventPostingDetail
-							( null,event.getTenantId(),event.getReferenceId(),event.getEventType(),
-									response.getStatusCode().toString(),null,record
-									,new Date(),new Date());
-					eventPostingDetailRepository.save(detail);  
+				} else {
+					EventPostingDetail detail = new EventPostingDetail(null, event.getTenantId(),
+							event.getReferenceId(), event.getEventType(), response.getStatusCode().toString(), null,
+							record, new Date(), new Date());
+					eventPostingDetailRepository.save(detail);
 				}
 
-
-			}catch(RuntimeException e)
-			{
-				String message=null;
-				if(e.getMessage().length() >4000)
-				{
-					message=e.getMessage().substring(0, 3999);
-				}else
-				{
-					message=e.getMessage();
-				}	
-				EventPostingDetail detail=new EventPostingDetail
-						( null,event.getTenantId(),event.getReferenceId(),event.getEventType(),"400",message,record,new Date(),new Date());
+			} catch (RuntimeException e) {
+				String message = null;
+				if (e.getMessage().length() > 4000) {
+					message = e.getMessage().substring(0, 3999);
+				} else {
+					message = e.getMessage();
+				}
+				EventPostingDetail detail = new EventPostingDetail(null, event.getTenantId(), event.getReferenceId(),
+						event.getEventType(), "400", message, record, new Date(), new Date());
 				eventPostingDetailRepository.save(detail);
 				break loop;
 			}
--- reference-adapter/src/main/java/org/egov/ifix/controller/EventController.java
+++ reference-adapter/src/main/java/org/egov/ifix/controller/EventController.java
@@ -1,7 +1,13 @@
 package org.egov.ifix.controller;
 
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.validation.Valid;
 
+import org.egov.common.contract.response.ResponseHeader;
+import org.egov.ifix.models.Event;
 import org.egov.ifix.models.EventRequest;
 import org.egov.ifix.models.EventResponse;
 import org.egov.ifix.service.EventService;
@@ -25,7 +31,24 @@ public class EventController {
 	public ResponseEntity<?>pushEvents( @Valid @RequestBody EventRequest eventRequest) {
 		log.info("request received");
 		eventService.pushEvent(eventRequest);
-		return new ResponseEntity<>(EventResponse.class, HttpStatus.CREATED);
+		EventResponse response=new EventResponse();
+		List<Event> events=new ArrayList<>();
+		events.add(eventRequest.getEvent());
+		response.setEvent(events); 
+		response.setResponseInfo(getResponseInfo(eventRequest));
+		
+		
+		return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+	}
+
+	private ResponseHeader getResponseInfo(EventRequest eventRequest) {
+		ResponseHeader responseInfo=new ResponseHeader();
+		responseInfo.setCorrelationId(eventRequest.getRequestHeader().getCorrelationId());
+		responseInfo.setTs(Instant.now().toEpochMilli());
+		responseInfo.setVersion(eventRequest.getRequestHeader().getVersion());
+		responseInfo.setStatus(HttpStatus.ACCEPTED.toString());
+		responseInfo.setMsgId(eventRequest.getRequestHeader().getMsgId());
+		return responseInfo;
 	}
 	
 }
