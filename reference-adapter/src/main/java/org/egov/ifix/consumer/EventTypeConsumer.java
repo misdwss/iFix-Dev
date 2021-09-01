6878f2db7fec9173c418092fff8f1395909fefda
Manikanta <manikanta@egovernments.org>
2021-09-01 17:46:36 +0530

    [IFIX-378]  implement expense bill push

--- reference-adapter/src/main/java/org/egov/ifix/consumer/EventTypeConsumer.java
+++ reference-adapter/src/main/java/org/egov/ifix/consumer/EventTypeConsumer.java
@@ -27,6 +27,7 @@ import org.springframework.web.client.RestClientException;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -75,96 +76,119 @@ public class EventTypeConsumer {
 
 	public void process(final String record, EventPostingDetail detail) {
 		log.info("Received Message from topic: " + record);
+		JsonObject jsonObject =null;
 		log.info("\n\n");
-		JsonObject jsonObject = JsonParser.parseString(record).getAsJsonObject();
-		EventMapper eventMapper = eventTypeMap.get(jsonObject.getAsJsonObject(EVENT).get(EVENT_TYPE).getAsString());
-		List<FiscalEvent> fiscalEvents = eventMapper.transformData(jsonObject);
-		FiscalEventRequest request = new FiscalEventRequest();
-		RequestHeader header = new RequestHeader();
-		polulateRequestHeader(jsonObject, header);
-		request.setRequestHeader(header);
+		try {
+			jsonObject = JsonParser.parseString(record).getAsJsonObject();
+			EventMapper eventMapper = eventTypeMap.get(jsonObject.getAsJsonObject(EVENT).get(EVENT_TYPE).getAsString());
+			List<FiscalEvent> fiscalEvents = eventMapper.transformData(jsonObject);
+			FiscalEventRequest request = new FiscalEventRequest();
+			RequestHeader header = new RequestHeader();
+			polulateRequestHeader(jsonObject, header);
+			request.setRequestHeader(header);
 
-		loop: for (FiscalEvent event : fiscalEvents) {
+			loop: for (FiscalEvent event : fiscalEvents) {
 
-			if (jsonObject.getAsJsonObject(EVENT).get("projectId") != null) {
-				event.setProjectId(jsonObject.getAsJsonObject(EVENT).get("projectId").getAsString());
-
-			}
-			request.setFiscalEvent(event);
-			log.info("Project Id" +event.getProjectId());
-
-			if (detail == null) {
-				detail = new EventPostingDetail();
-				if (jsonObject.getAsJsonObject(EVENT).get("id") != null) {
-					detail.setEventId(jsonObject.getAsJsonObject(EVENT).get("id").getAsString());
-					detail.setTenantId(event.getTenantId());
-					detail.setReferenceId(event.getParentReferenceId());
-					detail.setEventType(event.getEventType());
-					detail.setCreatedDate(new Date());
-					detail.setLastModifiedDate(new Date());
+				if (jsonObject.getAsJsonObject(EVENT).get("projectId") != null) {
+					event.setProjectId(jsonObject.getAsJsonObject(EVENT).get("projectId").getAsString());
 
 				}
-			}
+				request.setFiscalEvent(event);
+				log.info("Project Id" +event.getProjectId());
 
-			try {
+				if (detail == null) {
+					detail = new EventPostingDetail();
+					if (jsonObject.getAsJsonObject(EVENT).get("id") != null) {
+						detail.setEventId(jsonObject.getAsJsonObject(EVENT).get("id").getAsString());
+						detail.setTenantId(event.getTenantId());
+						detail.setReferenceId(event.getParentReferenceId());
+						detail.setEventType(event.getEventType());
+						detail.setCreatedDate(new Date());
+						detail.setLastModifiedDate(new Date());
 
-				ResponseEntity<FiscalEventResponse> response = postEvent.post(request);
+					}
+				}
 
-				if (response.getStatusCode().series().equals(HttpStatus.Series.SERVER_ERROR)) {
+				try {
 
-					detail.setStatus(String.valueOf(response.getStatusCode().value()));
+					ResponseEntity<FiscalEventResponse> response = postEvent.post(request);
+
+					if (response.getStatusCode().series().equals(HttpStatus.Series.SERVER_ERROR)) {
+
+						detail.setStatus(String.valueOf(response.getStatusCode().value()));
+						detail.setRecord(record);
+						eventPostingDetailRepository.save(detail);
+
+					}else if(response.getStatusCode().series().equals(HttpStatus.Series.CLIENT_ERROR)) {
+						detail.setStatus(String.valueOf(response.getStatusCode().value()));
+						detail.setRecord(record);
+						eventPostingDetailRepository.save(detail);
+
+					}
+					else if(response.getStatusCode().series().equals(HttpStatus.Series.SUCCESSFUL)) {
+						detail.setStatus(String.valueOf(response.getStatusCode().value()));
+						detail.setRecord(null);
+						eventPostingDetailRepository.save(detail);
+
+					} else  {
+						detail.setStatus(String.valueOf(response.getStatusCode().value()));
+						detail.setRecord(null);
+						eventPostingDetailRepository.save(detail);
+
+					}
+
+				} catch (ResourceAccessException e) {
+					String message = null;
+					if (e.getMessage().length() > 4000) {
+						message = e.getMessage().substring(0, 3999);
+					} else {
+						message = e.getMessage();
+					}
+					log.info(e.getMessage(), e);
+					detail.setStatus("500");
+					detail.setError(message);
+					detail.setRecord(record);
+					eventPostingDetailRepository.save(detail);
+					break loop;
+				}
+
+				catch (RestClientException e) {
+					log.info(e.getMessage(), e);
+					String message = null;
+					if (e.getMessage().length() > 4000) {
+						message = e.getMessage().substring(0, 3999);
+					} else {
+						message = e.getMessage();
+					}
+					log.info(e.getMessage(), e);
+					detail.setStatus("400");
+					detail.setError(message);
 					detail.setRecord(record);
 					eventPostingDetailRepository.save(detail);
 
-				}else if(response.getStatusCode().series().equals(HttpStatus.Series.CLIENT_ERROR)) {
-					detail.setStatus(String.valueOf(response.getStatusCode().value()));
-					detail.setRecord(record);
-					eventPostingDetailRepository.save(detail);
-
 				}
-				else if(response.getStatusCode().series().equals(HttpStatus.Series.SUCCESSFUL)) {
-					detail.setStatus(String.valueOf(response.getStatusCode().value()));
-					detail.setRecord(null);
-					eventPostingDetailRepository.save(detail);
-
-				} else  {
-					detail.setStatus(String.valueOf(response.getStatusCode().value()));
-					detail.setRecord(null);
-					eventPostingDetailRepository.save(detail);
-
-				}
-
-			} catch (ResourceAccessException e) {
-				String message = null;
-				if (e.getMessage().length() > 4000) {
-					message = e.getMessage().substring(0, 3999);
-				} else {
-					message = e.getMessage();
-				}
-				log.info(e.getMessage(), e);
-				detail.setStatus("500");
-				detail.setError(message);
-				detail.setRecord(record);
-				eventPostingDetailRepository.save(detail);
-				break loop;
-			}
-
-			catch (RestClientException e) {
-				log.info(e.getMessage(), e);
-				String message = null;
-				if (e.getMessage().length() > 4000) {
-					message = e.getMessage().substring(0, 3999);
-				} else {
-					message = e.getMessage();
-				}
-				log.info(e.getMessage(), e);
-				detail.setStatus("400");
-				detail.setError(message);
-				detail.setRecord(record);
-				eventPostingDetailRepository.save(detail);
 
 			}
-
+		} catch (Exception e) {
+			log.info(e.getMessage(), e);
+			String message = null;
+			if (e.getMessage().length() > 4000) {
+				message = e.getMessage().substring(0, 3999);
+			} else {
+				message = e.getMessage();
+			}
+			log.info(e.getMessage(), e);
+			EventPostingDetail	errorDetail = new EventPostingDetail();	
+			errorDetail.setEventId(jsonObject.getAsJsonObject(EVENT).get("id").getAsString());
+			errorDetail.setTenantId(jsonObject.getAsJsonObject(EVENT).get("tenantId").getAsString());
+			errorDetail.setReferenceId("Not extracted");
+			errorDetail.setEventType(jsonObject.getAsJsonObject(EVENT).get(EVENT_TYPE).getAsString());
+			errorDetail.setCreatedDate(new Date());
+			errorDetail.setLastModifiedDate(new Date());
+			errorDetail.setStatus("400");
+			errorDetail.setError(message);
+			errorDetail.setRecord(record);
+			eventPostingDetailRepository.save(errorDetail);
 		}
 
 	}
--- reference-adapter/src/main/java/org/egov/ifix/mapper/impl/bill/BillEventTypeImpl.java
+++ reference-adapter/src/main/java/org/egov/ifix/mapper/impl/bill/BillEventTypeImpl.java
@@ -1,28 +1,97 @@
 package org.egov.ifix.mapper.impl.bill;
 
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import org.egov.ifix.mapper.EventMapper;
+import org.egov.ifix.models.Amount;
 import org.egov.ifix.models.EventTypeEnum;
 import org.egov.ifix.models.FiscalEvent;
+import org.egov.ifix.utils.ApplicationConfiguration;
+import org.egov.ifix.utils.EventConstants;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
-@Component
-public class BillEventTypeImpl implements EventMapper{
+import lombok.extern.slf4j.Slf4j;
 
-	@Override
-	public List<FiscalEvent> transformData(JsonObject data) {
-		System.out.println("BillEventTypeImpl---");
-		return null;
+@Component
+@Slf4j
+public class BillEventTypeImpl implements EventMapper {
+
+	private static final String BILL = "bill";
+
+	private static final String REFERANCE_ID = "consumerCode";
+
+	private static final String BILL_DETAILS = "billDetails";
+
+	private static final String BILL_ACCOUNT_DETAILS = "billAccountDetails";
+
+	private static final String AMOUNT = "amount";
+
+	private static final String FROMPERIOD = "fromPeriod";
+
+	private static final String TOPERIOD = "toPeriod";
+
+	private ApplicationConfiguration applicationConfiguration;
+
+	@Autowired
+	public BillEventTypeImpl(ApplicationConfiguration applicationConfiguration) {
+		this.applicationConfiguration = applicationConfiguration;
 	}
 
 	@Override
 	public String getEventType() {
-		// TODO Auto-generated method stub
 		return EventTypeEnum.BILL.toString();
+	}
+
+	@Override
+	public List<FiscalEvent> transformData(JsonObject data) {
+		JsonArray bills = data.getAsJsonObject(EventConstants.EVENT).getAsJsonArray(EventConstants.ENTITY);
+		List<FiscalEvent> fiscalEvents = new ArrayList<FiscalEvent>();
+		for (int i = 0; i < bills.size(); i++) {
+			JsonObject bill = bills.get(i).getAsJsonObject().getAsJsonObject(BILL);
+
+			FiscalEvent fiscalEvent = FiscalEvent.builder().tenantId(applicationConfiguration.getTenantId())
+					.eventType(getEventType()).eventTime(Instant.now().toEpochMilli())
+					.referenceId(bill.get(REFERANCE_ID).getAsString()).parentEventId(null).parentReferenceId(null)
+					.amountDetails(getAmounts(bill)).build();
+
+			fiscalEvents.add(fiscalEvent);
+
+		}
+		return fiscalEvents;
+	}
+
+	private List<Amount> getAmounts(JsonObject demand) {
+
+		JsonArray billDetails = demand.getAsJsonArray(BILL_DETAILS);
+		Long fromPeriod=0l;
+		Long toPeriod=0l;
+		List<Amount> amounts = new ArrayList<Amount>();
+		for (int i = 0; i < billDetails.size(); i++) {
+
+			 fromPeriod = billDetails.get(i).getAsJsonObject().get(FROMPERIOD).getAsLong();
+			 toPeriod =  billDetails.get(i).getAsJsonObject().get(TOPERIOD).getAsLong();
+
+			JsonArray billAccountDetails = billDetails.get(i).getAsJsonObject().getAsJsonArray(BILL_ACCOUNT_DETAILS);
+
+			for (int j = 0; j < billAccountDetails.size(); j++) {
+				JsonObject demanddetail = billAccountDetails.get(j).getAsJsonObject();
+
+				Amount amount = Amount.builder().amount(demanddetail.get(AMOUNT).getAsBigDecimal()).coaId(null)
+						.fromBillingPeriod(toPeriod).toBillingPeriod(toPeriod).build();
+
+				amounts.add(amount);
+
+			}
+		}
+		return amounts;
+
 	}
 
 }
