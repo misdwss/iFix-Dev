02734c053992f23755f56504e45078b268bc0bf9
Manikanta <manikanta@egovernments.org>
2021-09-09 03:18:05 +0530

    [IFIX-378] Fix adjusted amount

--- reference-adapter/src/main/java/org/egov/ifix/mapper/impl/receipt/ReceiptEventTypeImpl.java
+++ reference-adapter/src/main/java/org/egov/ifix/mapper/impl/receipt/ReceiptEventTypeImpl.java
@@ -29,7 +29,7 @@ public class ReceiptEventTypeImpl implements EventMapper {
 
 	private static final String PAYMENT_DETAILS = "paymentDetails";
 
-	private static final String PAID_AMOUNT = "amount";
+	private static final String PAID_AMOUNT = "adjustedAmount";
 
 	private static final String TAX_PERIOD_FROM = "fromPeriod";
 
