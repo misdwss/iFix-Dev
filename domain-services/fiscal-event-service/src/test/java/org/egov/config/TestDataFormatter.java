b3c54aa44c92c2a66feabd316a14c26679080b38
pintu-eGov <87848739+pintu-eGov@users.noreply.github.com>
2021-10-05 13:05:13 +0530

    initial fiscal event junit changes (#101)
    * initial fiscal event junit changes
    
    * removed unused props
    
    Co-authored-by: pintu-eGov <pintu.kumar@egovernments.org>

---
+++ domain-services/fiscal-event-service/src/test/java/org/egov/config/TestDataFormatter.java
@@ -0,0 +1,31 @@
+package org.egov.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+
+@Component
+public class TestDataFormatter {
+    @Autowired
+    private TestProperties testProperties;
+
+    /**
+     * @param fileName
+     * @return
+     */
+    private File getFileFromClassLoaderResource(String fileName) {
+        String basePackage = testProperties.getTestDataPackage();
+        ClassLoader classLoader = this.getClass().getClassLoader();
+
+        if (basePackage != null && !basePackage.isEmpty()) {
+            String baseURL = classLoader.getResource(basePackage).getFile();
+            fileName = baseURL + File.separator + fileName;
+
+            return new File(fileName);
+        }else {
+            return new File(classLoader.getResource(fileName).getFile());
+        }
+    }
+
+}
