e5a2945103e0bf75046d3401e40248ff67cca794
Shashwat Mishra <71879793+shashwat-egov@users.noreply.github.com>
2022-09-01 15:47:56 +0530

    [DPG-698] Adding ifix-es-pipeline service (#117)

---
+++ domain-services/ifix-es-pipeline/src/main/java/org/egov/ifixespipeline/models/Ancestry.java
@@ -0,0 +1,19 @@
+package org.egov.ifixespipeline.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Ancestry {
+
+    @JsonProperty("code")
+    private String code = null;
+
+    @JsonProperty("type")
+    private String type = null;
+
+}
