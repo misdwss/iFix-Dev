367a2c16f79b8d982e8dc0cf2bd342d6485dc327
Shailesh <shailesh.kumar@egovernments.org>
2022-09-12 10:52:34 +0530

    [DPG-667] added proxy url of mgramseva

--- frontend/micro-ui/web/src/setupProxy.js
+++ frontend/micro-ui/web/src/setupProxy.js
@@ -1,6 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const createProxy = createProxyMiddleware({
-  target: process.env.REACT_APP_PROXY_URL,
+  // target: process.env.REACT_APP_PROXY_URL,
+  target: "https://mgramseva-qa.egov.org.in",
   changeOrigin: true,
 });
 module.exports = function (app) {
