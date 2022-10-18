2ecae9a536eff7a0d501ed669803101100f0ab04
Ghanshyam <ghanshyam@egovs-MacBook-Pro.local>
2022-10-18 11:22:31 +0530

    added coa query builder

--- domain-services/ifix-master-data-service/src/main/java/org/egov/repository/querybuilder/ChartOfAccountQueryBuilder.java
+++ domain-services/ifix-master-data-service/src/main/java/org/egov/repository/querybuilder/ChartOfAccountQueryBuilder.java
@@ -1,46 +1,93 @@
-package org.egov.repository.queryBuilder;
 
+package org.egov.repository.querybuilder;
 
-import lombok.extern.slf4j.Slf4j;
+import java.util.List;
+
 import org.apache.commons.lang3.StringUtils;
 import org.egov.web.models.COASearchCriteria;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.stereotype.Component;
+import
+
+org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Component
+
 @Slf4j
 public class ChartOfAccountQueryBuilder {
+	
+	
+	
+	public String buildSearchQuery(COASearchCriteria searchCriteria, List<Object> preparedStmtList) {
 
+		StringBuilder query = new StringBuilder("SELECT id, tenantid, coacode, majorhead, majorheadname, "
+				+ "submajorhead, submajorheadname, minorhead, minorheadname, subhead, subheadname, "
+				+ "grouphead, groupheadname, objecthead, objectheadname, attributes, "
+				+ "createdtime, createdby, lastmodifiedtime, lastmodifiedby "
+				+ "FROM eg_ifix_chartofaccount WHERE tenantid = ?");
+		preparedStmtList.add(searchCriteria.getTenantId());
 
-    public Query buildSearchQuery(COASearchCriteria searchCriteria) {
+		if (searchCriteria.getIds() != null && !searchCriteria.getIds().isEmpty()) {
+			query.append(" AND id IN ( ");
+			setValuesForList(query, preparedStmtList, searchCriteria.getIds());
+			query.append(")");
+		}
 
-        Criteria criteria = Criteria.where("tenantId").is(searchCriteria.getTenantId());
+		
+		if (StringUtils.isNotBlank(searchCriteria.getMajorHead())) {
+			query.append(" AND majorhead = ?");
+		preparedStmtList.add(searchCriteria.getMajorHead());
+		}
+		if (StringUtils.isNotBlank(searchCriteria.getMinorHead())) {
+			query.append(" AND minorhead = ?");
+			preparedStmtList.add(searchCriteria.getMinorHead());
+		}
 
-        if (StringUtils.isNotBlank(searchCriteria.getMajorHead()))
-            criteria.and("majorHead").is(searchCriteria.getMajorHead());
+		if (StringUtils.isNotBlank(searchCriteria.getGroupHead())) {
+			query.append(" AND grouphead = ?");
+		preparedStmtList.add(searchCriteria.getGroupHead());
+		}
 
-        if (StringUtils.isNotBlank(searchCriteria.getMinorHead()))
-            criteria.and("minorHead").is(searchCriteria.getMinorHead());
+		if (StringUtils.isNotBlank(searchCriteria.getObjectHead())) {
+			query.append(" AND objecthead = ?");
+			preparedStmtList.add(searchCriteria.getObjectHead());
+		
+		}
 
-        if (StringUtils.isNotBlank(searchCriteria.getGroupHead()))
-            criteria.and("groupHead").is(searchCriteria.getGroupHead());
+		if (StringUtils.isNotBlank(searchCriteria.getSubHead())) {
+			query.append(" AND subhead = ?");
+			preparedStmtList.add(searchCriteria.getSubHead());
+		}
 
-        if (StringUtils.isNotBlank(searchCriteria.getObjectHead()))
-            criteria.and("objectHead").is(searchCriteria.getObjectHead());
+		if (StringUtils.isNotBlank(searchCriteria.getSubMajorHead())) {
+			query.append(" AND submajorhead = ?");
+			preparedStmtList.add(searchCriteria.getSubMajorHead());
+		}
 
-        if (StringUtils.isNotBlank(searchCriteria.getSubHead()))
-            criteria.and("subHead").is(searchCriteria.getSubHead());
-
-        if (StringUtils.isNotBlank(searchCriteria.getSubMajorHead()))
-            criteria.and("subMajorHead").is(searchCriteria.getSubMajorHead());
-
-        if (searchCriteria.getCoaCodes() != null && !searchCriteria.getCoaCodes().isEmpty())
-            criteria.and("coaCode").in(searchCriteria.getCoaCodes());
-
-        if (searchCriteria.getIds() != null && !searchCriteria.getIds().isEmpty())
-            criteria.and("id").in(searchCriteria.getIds());
-
-        return new Query(criteria);
-    }
+		if (searchCriteria.getCoaCodes() != null && !searchCriteria.getCoaCodes().isEmpty()) {
+			query.append(" AND coacode IN ( ");
+			setValuesForList(query, preparedStmtList, searchCriteria.getCoaCodes());
+			query.append(")");
+		}
+		
+		return query.toString();
+	}
+	
+	
+	/**
+	 * Sets prepared statement for values for a list
+	 * 
+	 * @param query
+	 * @param preparedStmtList
+	 * @param ids
+	 */
+	private void setValuesForList(StringBuilder query, List<Object> preparedStmtList, List<String> ids) {
+		int len = ids.size();
+		for (int i = 0; i < ids.size(); i++) {
+			query.append("?");
+			if (i != len - 1)
+				query.append(", ");
+			preparedStmtList.add(ids.get(i));
+		}
+	}
 }
